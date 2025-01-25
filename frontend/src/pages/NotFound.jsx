fe9d78fe125af7526da17540c8c74fbcbb798c86
Patrick4350 <pbofah1@gmail.com>
2025-01-24 22:50:41 -0600

    Not Found Page updated!

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ function NotFound() {
     return (
         <div>
             <h1> 404 Not Found </h1>
-            <p> The page you're looking for does'nt exist! </p>
+            <p> The page you're looking for does not exist! </p>
         </div>
     )
 }
